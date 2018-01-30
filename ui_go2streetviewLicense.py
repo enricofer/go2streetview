68d67f26f5d1ccc1113fc6955126145352d72b93
Ferreguti Enrico <ferregutie@COMUNE.PADOVA.IT>
2018-01-30 14:59:10 +0100

    unneeded files removed

--- ui_go2streetviewLicense.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'C:\Users\ferregutie\Dropbox\dev\qgis2\go2streetview\ui_go2streetviewLicense.ui'
-#
-# Created: Mon Jan 08 10:51:28 2018
-#      by: PyQt4 UI code generator 4.10.2
-#
-# WARNING! All changes made in this file will be lost!
-
