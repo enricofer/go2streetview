59e56a58f4aff44cb5d38c01f4e49c11979e8587
enrico <enricofer@gmail.com>
2018-02-05 09:40:35 +0100

    release v7.5

--- go2streetview.py
+++ go2streetview.py
@@ -51,7 +51,7 @@ class go2streetview(QgsMapTool):
         self.iface = iface
         # reference to the canvas
         self.canvas = self.iface.mapCanvas()
-        self.version = 'v7.4.1'
+        self.version = 'v7.5'
         QgsMapTool.__init__(self, self.canvas)
         self.S = QSettings()
         terms = self.S.value("go2sv/license", defaultValue =  "undef")
